e79411b865525b260d54be840ab1765bbb2f044c
Ubuntu <ubuntu@ip-172-31-41-98.us-west-2.compute.internal>
2017-01-31 00:55:00 +0000

    Set up MYSQL server

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :ruby_win_sources
   get 'welcome_page/welcome'
   root 'welcome_page#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
